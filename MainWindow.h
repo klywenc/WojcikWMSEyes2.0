dc14c1ba0391ade3dc1b7e313e8cd95527daaa7a
Megu <43895321+klywenc@users.noreply.github.com>
2025-12-03 10:13:34 +0100

    Refactor MainWindow to include worker classes

--- MainWindow.h
+++ MainWindow.h
@@ -9,6 +9,10 @@
 #include <opencv2/opencv.hpp>
 #include <QtNetwork>
 #include <QQueue>
+#include <QThreadPool>
+#include <QRunnable>
+#include <QMutex>
+#include <QMap>
 #include "SettingDialog.h"
 
 struct UploadJob {
@@ -17,6 +21,50 @@ struct UploadJob {
     int camIndex;
 };
 
+class CameraWorker final : public QObject, public QRunnable {
+    Q_OBJECT
+public:
+    CameraWorker(int index, QString url, int protocolMode, int rotation,
+                 QString user, QString pass, QString savePath, QObject *parent = nullptr);
+    void run() override;
+
+signals:
+    void resultReady(int index, bool success, QString filePath, QString errorMsg);
+
+private:
+    int m_index;
+    QString m_url;
+    int m_protocol;
+    int m_rotation;
+    QString m_user;
+    QString m_pass;
+    QString m_savePath;
+};
+
+class UploadWorker : public QObject {
+    Q_OBJECT
+public:
+    explicit UploadWorker(QString serverUrl, int timeout, QObject *parent = nullptr);
+
+public slots:
+    void addJob(const UploadJob& job);
+    void processNext();
+
+signals:
+    void uploadStarted(int camIndex);
+    void uploadFinished(int camIndex, bool success, QString message);
+
+private:
+    void sendRequest(const UploadJob &job);
+
+    QNetworkAccessManager *manager;
+    QQueue<UploadJob> m_queue;
+    bool m_isUploading;
+    QString m_serverUrl;
+    int m_timeout;
+};
+
+// --- GŁÓWNE OKNO ---
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
@@ -28,55 +76,50 @@ protected:
     void keyPressEvent(QKeyEvent *event) override;
 
 private slots:
-    void captureSnapshot();
+    void startScanProcess();
     void openSettings();
     void handleSerialScan();
-    void startNextUpload();
-    void onUploadFinished(QNetworkReply *reply);
-    void updateUploadAnimation();
+    void updateClock() const;
+
+    void onCameraFinished(int index, bool success, const QString& filePath, const QString& errorMsg);
+    void onWorkerUploadStarted(int camIndex);
+    void onWorkerUploadFinished(int camIndex, bool success, const QString& message);
+
+signals:
+    void requestUpload(UploadJob job);
 
 private:
     void setupUi();
     void setupStyles();
-    void reloadCameras();
     void configureScanner();
-    void ensureTmpFolderExists();
+    static void ensureTmpFolderExists();
+    static QLabel* createCameraLabel(const QString &text);
 
-    QLabel* createCameraLabel(const QString &text);
-    void drawStatusOnImage(int camIndex, const QPixmap &basePix, QString text, QColor color, bool isAnimated = false);
+    // Status (0=Idle, 1=Sending, 2=OK, 3=Error)
+    void drawStatusOnImage(int camIndex, const QString &filePath, int status, const QString &msg = "");
 
     QWidget *centralWidget;
     QWidget *mainPanel;
     QLabel *logoLabel;
     QLabel *headerTitle;
     QLabel *dateLabel;
-
-    QLabel *cam1_TopLeft;
-    QLabel *cam4_TopRight;
-    QLabel *cam0_BotLeft;
-    QLabel *cam2_BotMid;
-    QLabel *cam3_BotRight;
+    QLabel *cam1_TopLeft; QLabel *cam4_TopRight; QLabel *cam0_BotLeft; QLabel *cam2_BotMid; QLabel *cam3_BotRight;
+    std::vector<QLabel*> camDisplays;
 
     SettingDialog *settingsDialog;
     QShortcut *secretShortcut;
     QShortcut *exitShortcut;
-
-    std::vector<cv::VideoCapture> captures;
-    std::vector<QLabel*> camDisplays;
     QTimer *clockTimer;
 
     QSerialPort *serialScanner;
     QByteArray serialBuffer;
-
-    QNetworkAccessManager *netManager;
-    QQueue<UploadJob> uploadQueue;
-    bool isUploading;
     QString currentPalletCode;
 
-    QTimer *animationTimer;
-    int spinnerAngle;
-    QPixmap currentBasePixmap;
-    int currentUploadCamIndex;
+    QString lastImagePaths[5];
+
+    QThreadPool *cameraPool;
+    QThread *uploadThread;
+    UploadWorker *uploadWorker;
 };
 
 #endif
